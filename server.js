c765d6c772b08949a54c66436f06ffe037679739
Danuka Chathurya <danukachathurya8@gmail.com>
2023-12-30 20:54:51 +0530

    Get rooms Backend

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
 const dbConfig = require('./db')
+const roomsRoute = require('./routes/roomsRoute')
+
+app.use('/api/rooms', roomsRoute)
 
 
 const port = process.env.PORT || 5000;
