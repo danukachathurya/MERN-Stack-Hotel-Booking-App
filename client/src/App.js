9b5636c63a7303ac1244b3591c0fdf3ea3c1030c
Danuka Chathurya <danukachathurya8@gmail.com>
2023-12-30 23:06:15 +0530

    Get rooms Front-End

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Homescreen from './screens/Homescreen';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/home" element={<Homescreen />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/screens/Homescreen.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function Homescreen() {
+  const [rooms, setRooms] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('/api/rooms/getallrooms');
+        setRooms(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setError('Error fetching data. Please try again.');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  return (
+    <div>
+      <h1>Home Screen</h1>
+      <h1>There are {rooms.length} rooms</h1>
+    </div>
+  );
+}
+
+export default Homescreen;
