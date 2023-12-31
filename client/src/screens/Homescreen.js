11ab37cb857b1494618a1f14a7b4e98339cacffe
Danuka Chathurya <danukachathurya8@gmail.com>
2023-12-31 16:56:01 +0530

    Fix some errors Get rooms Front-End

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -1,41 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, {useState, useEffect} from 'react'
+import axios from "axios";
 
 function Homescreen() {
-  const [rooms, setRooms] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('/api/rooms/getallrooms');
-        setRooms(response.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        setError('Error fetching data. Please try again.');
-      } finally {
-        setLoading(false);
-      }
-    };
+    const[rooms, setrooms] = useState([])
+    useEffect(() => {
+	const fetchData = async() => {
+ 	   try {
+            const response = await axios.get('/api/rooms/getallrooms');
+        setrooms(response.data);
 
-    fetchData();
-  }, []);
-
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
-  if (error) {
-    return <p>{error}</p>;
-  }
+        } catch (error) {
+            console.log(error)
+        }
+	};
+        fetchData();
+        
+    }, [])
 
   return (
     <div>
-      <h1>Home Screen</h1>
-      <h1>There are {rooms.length} rooms</h1>
+       <h1>Home Screen</h1>
+       <h1>there are {rooms.length} rooms</h1>
     </div>
-  );
+  )
 }
 
 export default Homescreen;
