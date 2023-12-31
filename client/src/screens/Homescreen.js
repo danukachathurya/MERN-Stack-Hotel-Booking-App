2d7916280ddb1a6e71bcbf808e3f4c8b5b922f26
Danuka Chathurya <danukachathurya8@gmail.com>
2023-12-31 18:52:01 +0530

    Design Homescreen

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -1,17 +1,26 @@
 import React, {useState, useEffect} from 'react'
 import axios from "axios";
+import Room from '../components/Room';
+
 
 function Homescreen() {
 
     const[rooms, setrooms] = useState([])
+    const[loading, setloading] = useState()
+    const[error, seterror] = useState()
     useEffect(() => {
 	const fetchData = async() => {
  	   try {
+            setloading(true)
             const response = await axios.get('/api/rooms/getallrooms');
-        setrooms(response.data);
+
+            setrooms(response.data);
+            setloading(false)
 
         } catch (error) {
+            seterror(true)
             console.log(error)
+            setloading(false)
         }
 	};
         fetchData();
@@ -19,9 +28,16 @@ function Homescreen() {
     }, [])
 
   return (
-    <div>
-       <h1>Home Screen</h1>
-       <h1>there are {rooms.length} rooms</h1>
+    <div className='coontainer'>
+        <div className="row justify-content-center mt-5">
+          {loading ? (<h1>Loading....</h1>) : error ? (<h1>Error</h1>) : (rooms.map(room=>{
+
+          return <div className="col-md-9 mt-2">
+            <Room room={room}/>
+          </div>
+
+          }))}
+        </div>
     </div>
   )
 }
