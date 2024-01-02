4877747139a468ce8d10ff606ae1629946856dd8
Danuka Chathurya <danukachathurya8@gmail.com>
2024-01-02 15:40:33 +0530

    Fix some errors Booking Room Screen Backend

--- client/src/screens/Bookingscreen.js
+++ client/src/screens/Bookingscreen.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
+import axios from "axios";
 
 function Bookingscreen() {
   const { roomid } = useParams();
   console.log(roomid);
+
+  const [loading, setloading] = useState();
+  const [error, seterror] = useState();
+  const [room, setroom] = useState();
+
+  useEffect(() => {
+    const fetchData = async() => {
+
+      try {
+        setloading(true);
+        const data = (await axios.post("/api/rooms/getroombyid", {roomid : roomid})).data;
+        setroom(data);
+        setloading(false);
+      } catch (error) {
+        setloading(false);
+        seterror(true);
+      }
+    };
+    fetchData();
+  }, [])
+
   return (
     <div>
       <h1>Booking Screen</h1>
