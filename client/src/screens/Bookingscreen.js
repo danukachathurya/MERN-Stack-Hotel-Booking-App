84b732ddb6f8cc696703eca1202b8a59105b59dd
Danuka Chathurya <danukachathurya8@gmail.com>
2024-01-05 03:04:29 +0530

    Setup payment front-end with stripe

--- client/src/screens/Bookingscreen.js
+++ client/src/screens/Bookingscreen.js
@@ -1,9 +1,10 @@
-import React, {useState, useEffect} from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import axios from "axios";
-import Loader from '../components/Loader';
-import Error from '../components/Error';
-import moment from 'moment';
+import Loader from "../components/Loader";
+import Error from "../components/Error";
+import moment from "moment";
+import StripeCheckout from "react-stripe-checkout";
 
 function Bookingscreen() {
   const { roomid, fromdate, todate } = useParams();
@@ -13,20 +14,19 @@ function Bookingscreen() {
   const [error, seterror] = useState();
   const [room, setroom] = useState();
 
-  const fromDate = moment(fromdate, 'DD-MM-YYYY');
-  const toDate = moment(todate, 'DD-MM-YYYY');
+  const fromDate = moment(fromdate, "DD-MM-YYYY");
+  const toDate = moment(todate, "DD-MM-YYYY");
 
-  const totaldays = moment.duration(toDate.diff(fromDate)).asDays()+1;
+  const totaldays = moment.duration(toDate.diff(fromDate)).asDays() + 1;
   const [totalamount, settotalamount] = useState();
 
-  
-
   useEffect(() => {
-    const fetchData = async() => {
-
+    const fetchData = async () => {
       try {
         setloading(true);
-        const data = (await axios.post("/api/rooms/getroombyid", {roomid : roomid})).data;
+        const data = (
+          await axios.post("/api/rooms/getroombyid", { roomid: roomid })
+        ).data;
         settotalamount(data.rentperday * totaldays);
         setroom(data);
         setloading(false);
@@ -36,73 +36,81 @@ function Bookingscreen() {
       }
     };
     fetchData();
-  }, [])
+  }, []);
 
-  async function bookRoom() {
+  async function onToken(token) {
+    console.log(token);
 
     const bookingDetails = {
-
       room,
-      userid:JSON.parse(localStorage.getItem('currentUser'))._id,
+      userid: JSON.parse(localStorage.getItem("currentUser"))._id,
       fromdate,
       todate,
       totalamount,
-      totaldays
-    }
-    
-    try {
-      const result = await axios.post('/api/booking/bookroom',bookingDetails)
-    } catch (error) {
-      
-    }
+      totaldays,
+      token
+    };
 
+    try {
+      const result = await axios.post("/api/booking/bookroom", bookingDetails);
+    } catch (error) {}
   }
 
   return (
     <div className="m-5">
-      {loading ? (<Loader/>) : room ? (<div>
+      {loading ? (
+        <Loader />
+      ) : room ? (
+        <div>
+          <div className="row justify-content-center mt-5 bs">
+            <div className="col-md-6">
+              <h1>{room.name}</h1>
+              <img src={room.imageurls[0]} className="bigimg" />
+            </div>
 
-        <div className="row justify-content-center mt-5 bs">
+            <div className="col-md-6">
+              <div style={{ textAlign: "right" }}>
+                <h1>Booking Details</h1>
+                <hr />
 
-          <div className="col-md-6">
-            <h1>{room.name}</h1>
-            <img src={room.imageurls[0]} className="bigimg"/>
+                <b>
+                  <p>
+                    Name :{" "}
+                    {JSON.parse(localStorage.getItem("currentUser")).name}
+                  </p>
+                  <p>From Date : {fromdate}</p>
+                  <p>To Date : {todate}</p>
+                  <p>Max Count : {room.maxcount}</p>
+                </b>
+              </div>
+
+              <div style={{ textAlign: "right" }}>
+                <h1>Amount</h1>
+                <hr />
+
+                <b>
+                  <p>Total days : {totaldays}</p>
+                  <p>Rent per day : {room.rentperday}</p>
+                  <p>Total Amount : {totalamount}</p>
+                </b>
+              </div>
+
+              <div style={{ float: "right" }}>
+                <StripeCheckout
+                  amount={totalamount * 100}
+                  token={onToken}
+                  currency="USD"
+                  stripeKey="pk_test_51OUxf7BkRKQHJh5bxkfhTarqoicTflibhRCxme4CjfoZd39TB9uEGD2bS1NL3F5iWsmzuD5BVPDBuPZXOY5ZlRUy00ATvv7YhL"
+                >
+                  <button className="btn btn-primary">Pay Now</button>
+                </StripeCheckout>
+              </div>
+            </div>
           </div>
-
-          <div className="col-md-6">
-            
-            <div style={{textAlign:'right'}}>
-              <h1>Booking Details</h1>
-              <hr/>
-
-              <b>
-                <p>Name : {JSON.parse(localStorage.getItem('currentUser')).name}</p>
-                <p>From Date : {fromdate}</p>
-                <p>To Date : {todate}</p>
-                <p>Max Count : {room.maxcount}</p>
-              </b>
-            </div>
-
-            <div style={{textAlign:'right'}}>
-              <h1>Amount</h1>
-              <hr/>
-
-              <b>
-                <p>Total days : {totaldays}</p>
-                <p>Rent per day : {room.rentperday}</p>
-                <p>Total Amount : {totalamount}</p>
-              </b>
-            </div>
-
-            <div style={{float:'right'}}>
-              <button className='btn btn-primary' onClick={bookRoom}>Pay Now</button>
-            </div>
-
-          </div>
-
         </div>
-
-      </div>) : (<Error/>)}
+      ) : (
+        <Error />
+      )}
     </div>
   );
 }
