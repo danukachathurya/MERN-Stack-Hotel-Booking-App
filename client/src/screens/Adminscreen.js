9f2e088f26c61c4bd7745baa872f9060c8a2451a
Danuka Chathurya <danukachathurya8@gmail.com>
2024-01-07 01:39:15 +0530

    Get all bookings frontend

--- client/src/screens/Adminscreen.js
+++ client/src/screens/Adminscreen.js
@@ -20,7 +20,7 @@ function Adminscreen() {
           <Bookings />
         </TabPane>
         <TabPane tab="Rooms" key="2">
-          <h1>Rooms</h1>
+          <Rooms />
         </TabPane>
         <TabPane tab="Add Room" key="3">
           <h1>Add Room</h1>
@@ -42,26 +42,165 @@ export function Bookings() {
 
   useEffect(() => {
     const fetchData = async () => {
-    try {
-      const data = await (await axios.get("/api/booking/getallbookings")).data;
-      setbookings(data);
-      setloading(false);
-    } catch (error) {
-      console.log(error);
-      setloading(false);
-      seterror(error);
-    }
-  };
-  fetchData();  
+      try {
+        const data = await (
+          await axios.get("/api/booking/getallbookings")
+        ).data;
+        setbookings(data);
+        setloading(false);
+      } catch (error) {
+        console.log(error);
+        setloading(false);
+        seterror(error);
+      }
+    };
+    fetchData();
   }, []);
 
   return (
     <div className="row">
-      <div className="col-md-10">
+      <div className="col-md-12">
         <h1>Bookings</h1>
         {loading && <Loader />}
+
+        <table className="table table-bordered table-dark">
+          <thead className="bs">
+            <tr>
+              <th>Booking Id</th>
+              <th>User Id</th>
+              <th>Room</th>
+              <th>From</th>
+              <th>To</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {bookings.length &&
+              bookings.map((booking) => {
+                return (
+                  <tr>
+                    <td>{booking._id}</td>
+                    <td>{booking.userid}</td>
+                    <td>{booking.room}</td>
+                    <td>{booking.fromdate}</td>
+                    <td>{booking.todate}</td>
+                    <td>{booking.status}</td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+
         {bookings.length && <h1>There are total {bookings.length} bookings</h1>}
       </div>
     </div>
   );
+}
+
+export function Rooms() {
+  const [rooms, setrooms] = useState([]);
+  const [loading, setloading] = useState(true);
+  const [error, seterror] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await (await axios.get("/api/rooms/getallrooms")).data;
+        setrooms(data);
+        setloading(false);
+      } catch (error) {
+        console.log(error);
+        setloading(false);
+        seterror(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="row">
+      <div className="col-md-12">
+        <h1>Rooms</h1>
+        {loading && <Loader />}
+
+        <table className="table table-bordered table-dark">
+          <thead className="bs">
+            <tr>
+              <th>Room Id</th>
+              <th>Name</th>
+              <th>Type</th>
+              <th>Rent Per Day</th>
+              <th>Max Count</th>
+              <th>Phone Number</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {rooms.length &&
+              rooms.map((room) => {
+                return (
+                  <tr>
+                    <td>{room._id}</td>
+                    <td>{room.name}</td>
+                    <td>{room.type}</td>
+                    <td>{room.rentperday}</td>
+                    <td>{room.maxcount}</td>
+                    <td>{room.phonenumber}</td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+
+        {rooms.length && <h1>There are total {rooms.length} rooms</h1>}
+      </div>
+    </div>
+  );
+}
+
+export function Users() {
+
+  const [users, setusers] = useState([]);
+  const [loading, setloading] = useState(true);
+  const [error, seterror] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await (await axios.get("/api/users/getallusers")).data;
+        setusers(data);
+        setloading(false);
+      } catch (error) {
+        console.log(error);
+        setloading(false);
+        seterror(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="row">
+      <div className="col-md-12">
+
+        <h1>Users</h1>
+
+        <table className='table table-dark table-bordered'>
+
+          <thead>
+            <tr>
+              <th>User Id</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Is Admin</th>
+            </tr>
+          </thead>
+
+        </table>
+
+      </div>  
+    </div>
+  )
+
 }
