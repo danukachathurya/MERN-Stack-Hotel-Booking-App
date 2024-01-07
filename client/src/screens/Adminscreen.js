5d44bb7f45be054865a5c7641ff76c3d38522ba7
Danuka Chathurya <danukachathurya8@gmail.com>
2024-01-07 08:19:13 +0530

    Create add new room - front-end and back-end

--- client/src/screens/Adminscreen.js
+++ client/src/screens/Adminscreen.js
@@ -3,18 +3,16 @@ import { Tabs } from "antd";
 import axios from "axios";
 import Loader from "../components/Loader";
 import Error from "../components/Error";
+import Swal from "sweetalert2";
 
 const { TabPane } = Tabs;
 
 function Adminscreen() {
-
   useEffect(() => {
-
-    if(!JSON.parse(localStorage.getItem("currentUser")).isAdmin) {
-      window.location.href = "/home"
+    if (!JSON.parse(localStorage.getItem("currentUser")).isAdmin) {
+      window.location.href = "/home";
     }
-
-  }, [])
+  }, []);
 
   return (
     <div
@@ -32,7 +30,7 @@ function Adminscreen() {
           <Rooms />
         </TabPane>
         <TabPane tab="Add Room" key="3">
-          <h1>Add Room</h1>
+          <Addroom />
         </TabPane>
         <TabPane tab="Users" key="4">
           <Users />
@@ -44,6 +42,7 @@ function Adminscreen() {
 
 export default Adminscreen;
 
+// Booking list Component
 export function Bookings() {
   const [bookings, setbookings] = useState([]);
   const [loading, setloading] = useState(true);
@@ -107,6 +106,7 @@ export function Bookings() {
   );
 }
 
+// Room list Component
 export function Rooms() {
   const [rooms, setrooms] = useState([]);
   const [loading, setloading] = useState(true);
@@ -168,8 +168,8 @@ export function Rooms() {
   );
 }
 
+// Users list Component
 export function Users() {
-
   const [users, setusers] = useState([]);
   const [loading, setloading] = useState(true);
   const [error, seterror] = useState();
@@ -192,11 +192,9 @@ export function Users() {
   return (
     <div className="row">
       <div className="col-md-12">
-
         <h1>Users</h1>
         {loading && <Loader />}
-        <table className='table table-dark table-bordered'>
-
+        <table className="table table-dark table-bordered">
           <thead>
             <tr>
               <th>User Id</th>
@@ -207,20 +205,94 @@ export function Users() {
           </thead>
 
           <tbody>
-            {users && (users.map(user => {
-              return <tr>
-                <td>{user._id}</td>
-                <td>{user.name}</td>
-                <td>{user.email}</td>
-                <td>{user.isAdmin ? 'YES' : 'NO'}</td>
-              </tr>
-            }))}
+            {users &&
+              users.map((user) => {
+                return (
+                  <tr>
+                    <td>{user._id}</td>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>{user.isAdmin ? "YES" : "NO"}</td>
+                  </tr>
+                );
+              })}
           </tbody>
-
         </table>
+      </div>
+    </div>
+  );
+}
 
-      </div>  
+
+// Add Room Component
+
+export function Addroom() {
+
+  const[name, setname] = useState('')
+  const[rentperday, setrentperday] = useState()
+  const[maxcount, setmaxcount] = useState()
+  const[description, setdescription] = useState()
+  const[phonenumber, setphonenumber] = useState()
+  const[type, settype] = useState()
+  const[imageurl1, setimageurl1] = useState()
+  const[imageurl2, setimageurl2] = useState()
+  const[imageurl3, setimageurl3] = useState()
+
+  const [loading, setloading] = useState(false);
+  const [error, seterror] = useState();
+
+  async function addRoom() {
+
+    const newroom = {
+      name,
+      rentperday,
+      maxcount,
+      description,
+      phonenumber,
+      type,
+      imageurls:[imageurl1, imageurl2, imageurl3]
+    }
+    try {
+      setloading(true);
+      const result = await (await axios.post('/api/rooms/addroom', newroom)).data
+      console.log(result)
+      setloading(false);
+      Swal.fire('Congrats', "Your New Room Added Successfully", 'Success').then(result => {
+        window.location.href='/home'
+      })
+    } catch (error) {
+      console.log(error)
+      setloading(false);
+      Swal.fire('OOps', 'Something went wrong', 'error')
+    }
+  }
+
+  return (
+    <div className='row'>
+        
+      <div className="col-md-5">
+        {loading && <Loader />}
+        <input type="text" className='form-control' placeholder="room name" value={name} onChange={(e) => {setname(e.target.value)}}/>
+        <input type="text" className='form-control' placeholder="rent per day" value={rentperday} onChange={(e) => {setrentperday(e.target.value)}}/>
+        <input type="text" className='form-control' placeholder="max count" value={maxcount} onChange={(e) => {setmaxcount(e.target.value)}}/>
+        <input type="text" className='form-control' placeholder="description" value={description} onChange={(e) => {setdescription(e.target.value)}}/>
+        <input type="text" className='form-control' placeholder="phone number" value={phonenumber} onChange={(e) => {setphonenumber(e.target.value)}}/>
+
+      </div>
+
+      <div className="col-md-5">
+
+        <input type="text" className='form-control' placeholder="type" value={type} onChange={(e) => {settype(e.target.value)}}/>
+        <input type="text" className='form-control' placeholder="Image URL 1" value={imageurl1} onChange={(e) => {setimageurl1(e.target.value)}}/>
+        <input type="text" className='form-control' placeholder="Image URL 2" value={imageurl2} onChange={(e) => {setimageurl2(e.target.value)}}/>
+        <input type="text" className='form-control' placeholder="Image URL 3" value={imageurl3} onChange={(e) => {setimageurl3(e.target.value)}}/>
+
+        <diV>
+          <button className="btn btn-primary mt-2" onClick={addRoom}>Add Room</button>
+        </diV>
+
+      </div>
+
     </div>
   )
-
 }
