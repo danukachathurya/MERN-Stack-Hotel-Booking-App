00dd0b488c282c16a36770a5f64d701b15f22eb3
Danuka Chathurya <danukachathurya8@gmail.com>
2024-01-08 02:23:39 +0530

    Designing Landing Page

---
+++ client/src/screens/Landingscreen.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+function Landingscreen() {
+  return (
+    <div className='row landing justify-content-center'>
+
+      <div className="col-md-9 my-auto" style={{textAlign:'center', borderRight: '8px solid white'}}>
+
+        <h2 style={{color:'white', fontSize: '130px'}}>SheyRooms</h2>
+        <h1 style={{color:'white'}}>"There is only one boss. The Guest."</h1>
+
+        <Link to='/home'>
+          <button className='btn landingbtn'>Get Started</button>
+        </Link>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Landingscreen;
